d360d310a8f8a9f1aa8079d0bd7d1defaee3e6c7
eng618 <eng618@fullsail.edu>
2014-02-18 21:14:21 -0500

    Initial Commit

---
+++ 1402MDF2Project3Tests/_402MDF2Project3Tests.m
@@ -0,0 +1,34 @@
+//
+//  _402MDF2Project3Tests.m
+//  1402MDF2Project3Tests
+//
+//  Created by Eric Garcia on 2/18/14.
+//  Copyright (c) 2014 Full Sail. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _402MDF2Project3Tests : XCTestCase
+
+@end
+
+@implementation _402MDF2Project3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
