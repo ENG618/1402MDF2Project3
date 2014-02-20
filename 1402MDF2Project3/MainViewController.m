0187b7b6b3c6360874e0819b1e5efc6fbf1dfe22
eng618 <eng618@fullsail.edu>
2014-02-20 08:34:22 -0500

    Added code for video capture. Added alert view to show saved successful

--- 1402MDF2Project3/MainViewController.m
+++ 1402MDF2Project3/MainViewController.m
@@ -33,30 +33,35 @@
 - (IBAction)photoButton:(id)sender
 {
     //Instanciate picker controll
-    UIImagePickerController *capturePhoto = [[UIImagePickerController alloc] init];
-    if (capturePhoto != nil) {
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+    if (imagePicker != nil) {
         //Set type of control
-        capturePhoto.sourceType = UIImagePickerControllerSourceTypeCamera;
+        imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
         //Set delagate
-        capturePhoto.delegate = self;
+        imagePicker.delegate = self;
         //Set editing
-        capturePhoto.allowsEditing = YES;
+        imagePicker.allowsEditing = YES;
         //Show camera
-        [self presentViewController:capturePhoto animated:YES completion:^(void){
-//            if (error == nil) {
-//                NSLog(@"There was an error: %@", error.description);
-//            }else{
-//                NSLog(@"Photo was captured and saved succesfully");
-//            }
+        [self presentViewController:imagePicker animated:YES completion:^(void){
+            //[self prepareForSegueWithIdentifier:@"imageSegue" sender:self];
         }];
-        
     }
     
 }
 
 - (IBAction)videoButton:(id)sender
 {
-    
+    UIImagePickerController *videoPicker = [[UIImagePickerController alloc]init];
+    if (videoPicker != nil) {
+        videoPicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        videoPicker.delegate = self;
+        videoPicker.allowsEditing = NO;
+        videoPicker.videoQuality = UIImagePickerControllerQualityTypeHigh;
+        
+        videoPicker.mediaTypes = [NSArray arrayWithObjects:(NSString *) kUTTypeMovie, nil];
+        
+        [self presentViewController:videoPicker animated:YES completion:nil];
+    }
 }
 
 - (IBAction)albumButton:(id)sender
@@ -69,26 +74,36 @@
         
         albumPicker.allowsEditing = YES;
         
-        [self presentViewController:albumPicker animated:YES completion:NULL];
-    } 
+        [self presentViewController:albumPicker animated:YES completion:nil];
+    }
 }
 
 #pragma mark - Picker Controll
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
 {
-    UIImage *selectedImage = [info objectForKey:@"UIImagePickerControllerOriginalImage"];
-    if (selectedImage) {
-        self.photoImageView.image = selectedImage;
-        
-    }
-    UIImage *scaledImage = [info objectForKey:@"UIImagePickerControllerEditedImage"];
-    if (scaledImage) {
-        self.scaledImageView.image = scaledImage;
-        
-        //Save image to photo album
-        UIImageWriteToSavedPhotosAlbum(scaledImage, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
-        
+    if ([[info objectForKey:UIImagePickerControllerMediaType]  isEqual: @"public.image"]) {
+        //Obtain original image
+        UIImage *selectedImage = [info objectForKey:@"UIImagePickerControllerOriginalImage"];
+        if (selectedImage) {
+            self.photoImageView.image = selectedImage;
+        }
+        //Obtain scaled image
+        UIImage *scaledImage = [info objectForKey:@"UIImagePickerControllerEditedImage"];
+        if (scaledImage) {
+            self.scaledImageView.image = scaledImage;
+            
+            //Save image to photo album
+            UIImageWriteToSavedPhotosAlbum(scaledImage, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+        }
+    }else if ([[info objectForKey:UIImagePickerControllerMediaType]  isEqual: @"public.movie"]){
+        //Obtain url for video
+        NSURL *urlString = [info valueForKey:UIImagePickerControllerMediaURL];
+        if (urlString) {
+            //Convert video url to a path
+            NSString *videoPath = [urlString path];
+            NSLog(@"%@", videoPath);
+        }
     }
     
     [picker dismissViewControllerAnimated:YES completion:nil];
@@ -103,7 +118,22 @@
     if (error != nil) {
         NSLog(@"Error: %@", [error description]);
     }else{
+        UIAlertView *saveSuccess = [[UIAlertView alloc] initWithTitle:@"Save successful"
+                                                              message:nil
+                                                             delegate:nil
+                                                    cancelButtonTitle:nil
+                                                    otherButtonTitles:@"Okay", nil];
+        [saveSuccess show];
         NSLog(@"Save was successful");
+    }
+}
+
+#pragma mark - Navigation
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if (segue) {
+        
     }
 }
 
